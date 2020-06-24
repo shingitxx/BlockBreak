39695a8f0153e5316cfa837336319f4dfed82c56
shingitxx <nextstagexx@gmail.com>
2020-06-24 21:31:50 +0900

    messege

--- js/main.js
+++ js/main.js
@@ -19,8 +19,76 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false; //右の矢印のフラグ 押されてなかったらfalse
 let leftPressed = false; //左の矢印のフラグ 押されてなかったらfalse
 
+//スコアの操作
+let score = 0;
+//プレーヤーのライフ
+let lives = 3;
+
+//ブロック変数
+let brickRowCount = 3; //ブロックの縦の数
+let brickColumnCount = 5; //ブロックの横の数
+let brickWidth = 75; //ブロックの幅
+let brickHeight = 20; //ブロックの高さ
+let brickPadding = 10; //ブロックの内側
+let brickOffsetTop = 30; //ブロックの画面上に配置したhtml要素の表示位置を座標で取得(Top)
+let brickOffsetLeft = 30; //ブロックの画面上に配置したhtml要素の表示位置を座標で取得(Left)
+
+let bricks = []; //ブロック、レンガ
+
+// 2重構文
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+//ブロックの衝突
+function collisionDetection() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          //ボールの位置の横幅を考慮とした範囲内 y座標が縦幅を考慮した範囲内
+          b.status = 0;
+          score++; //ブロックにぶつかる事に計算される
+
+          //ブロック成功で勝利メッセージを表示
+          if (score == brickRowCount * brickColumnCount) {
+            //掛けてブロックの数が全て無くなったら
+            alert("YOU WIN, CONGRATULATIONS!"); //アラートで内容を表示する
+            document.location.reload(); //再表示
+          }
+        }
+      }
+    }
+  }
+}
+
+//スコアの計算
+function drawScore() {
+  ctx.font = "16px Arial"; //スコアの文字
+  ctx.fillStyle = "#0095DD"; //スコアのカラー 図形の内側を塗りつぶす
+  ctx.fillText("Score:" + score, 8, 20); //スコアのテキストの変更
+}
+
+//ライフの計算
+function drawLives() {
+  ctx.font = "16px Arial"; //スコアの文字
+  ctx.fillStyle = "#0095DD"; //スコアのカラー 図形の内側を塗りつぶす
+  ctx.fillText("lives:" + lives, canvas.width - 65, 8, 20); //スコアのテキストの変更
+}
+
+//ボールを描画する部分
 function drawBall() {
-  //ボールを描画する部分
   ctx.beginPath(); //現在のパスをリセットする
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false); //ctxのサイズ 円弧の作成に用いる
   ctx.fillStyle = "#0095DD"; //ctxのカラー
@@ -28,8 +96,8 @@ function drawBall() {
   ctx.closePath(); //最終座標と開始座標を結び閉じる
 }
 
+//ボールをぶつける板
 function drawPaddle() {
-  //ボールをぶつける板
   ctx.beginPath(); //現在のパスをリセットする
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight); //ctxのサイズ 四角形の作成に用いる
   ctx.fillStyle = "#0095DD"; //ctxのカラー
@@ -37,14 +105,63 @@ function drawPaddle() {
   ctx.closePath(); //最終座標と開始座標を結び閉じる
 }
 
+//崩すブロック関係 ブロック描画関数
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    //cよりブロックの横の数が大きい時1増える
+    for (let r = 0; r < brickRowCount; r++) {
+      //rよりブロックの縦の数が大きい時1増える
+
+      if (bricks[c][r].status == 1) {
+        let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX; //bricks レンガ x方向
+        bricks[c][r].y = brickY; //bricks レンガ y方向
+
+        ctx.beginPath(); //現在のパスをリセットする
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD"; //ctxのカラー
+        ctx.fill(); //塗り潰し
+        ctx.closePath(); //最終座標と開始座標を結び閉じる
+      }
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); //フレーム削除(線が球体になる)
   drawBall(); //ボールを描画する部分を実行
+  drawBricks();
   drawPaddle(); //ボールをぶつける板を実行
+  drawScore(); //スコアの実行
+  drawLives(); //プレーヤーのライフの実行
+  collisionDetection();
 
-  if ((y + dy > canvas.height - ballRadius) | (y + dy < ballRadius)) {
+  if (y + dy < ballRadius) {
+    //衝突検知 ボールの半径
     //上下の壁を作成 ボールを閉じ込める
     dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    //ボールが下についた場合
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      if ((y = y - paddleHeight)) {
+        dy = -dy;
+      }
+    } else {
+      lives--;
+
+      if (!lives) {
+        alert("GAME OVER"); //アラートでゲームオーバーとなる
+        document.location.reload(); //再リロード
+        clearInterval(interval); //interval解除
+      } else {
+        x = canvas.width / 2;
+        y = canvas.height - 30;
+        dx = 3;
+        dy = -3;
+        paddleX = (canvas.width - paddleWidth) / 2;
+      }
+    }
   }
 
   if ((x + dx > canvas.width - ballRadius) | (x + dx < ballRadius)) {
@@ -53,59 +170,65 @@ function draw() {
   }
 
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
-    paddleX += 7; //右を押したらパドルを7動かす
+    paddleX += 10; //右を押したらパドルを7動かす
     //paddleXの範囲もcanvas内として定義する
+    rightPressed = false; //パドルが永遠にループしてしまうのを阻止する
   } else if (leftPressed && paddleX > 0) {
-    paddleX -= 7; //左を押したらパドルを7動かす
+    paddleX -= 10; //左を押したらパドルを7動かす
     //paddleXの範囲もcanvas内として定義する
+    leftPressed = false; //パドルが永遠にループしてしまうのを阻止する
   }
-
   x += dx; //右方向
   y += dy; //下方向
   //xとyにマイフレーム描画した後に小さな値を加えボールを動いているようにする
+
+  requestAnimationFrame(draw); //滑らかにアニメーションができる
 }
 
 document.addEventListener("keydown", keyDownHandler, false); //keyDownが押されたら反応する
 document.addEventListener("keyUp", keyUpHandler, false); //keyUpが押されたら反応する
+document.addEventListener("mousemove", mousemoveHandler, false); //パドルをマウスで操作
 
 function keyDownHandler(e) {
-  // keyDown ボタンを押している時
   if (e.key == "Right" || e.key == "ArrowRight") {
-    //右矢印ArrowRight 右矢印が押されたらtrue
     rightPressed = true;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
-    //左矢印ArrowLeft 左矢印が押されたらfalse
     leftPressed = true;
   }
 }
-
 function keyUpHandler(e) {
-  //keyUp ボタンを押していない時
   if (e.key == "Right" || e.key == "ArrowRight") {
-    //左矢印ArrowRight 左矢印が押されたらfalse
     rightPressed = false;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
-    //左矢印ArrowRight 左矢印が押されたらfalse
     leftPressed = false;
   }
 }
 
-setInterval(draw, 10); //コマ送りのような感じなのでsetIntervalを使う 10ミリ秒で動く
+function mousemoveHandler(e) {
+  let relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
 
-ctx.beginPath(); //現在のパスをリセットする
-ctx.rect(20, 40, 50, 50); //ctxのサイズ rect 四角形を作成に用いる
-ctx.fillStyle = "#FF0000"; //ctxのカラー
-ctx.fill(); //塗り潰し
-ctx.closePath(); //最終座標と開始座標を結び閉じる
+// let interval = setInterval(draw, 10); //コマ送りのような感じなのでsetIntervalを使う 10ミリ秒で動く
 
-ctx.beginPath(); //現在のパスをリセットする
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false); //ctxのサイズ 円弧の作成に用いる
-ctx.fillStyle = "green"; //ctxのカラー
-ctx.fill(); //塗り潰し
-ctx.closePath(); //最終座標と開始座標を結び閉じる
+draw();
 
-ctx.beginPath(); //現在のパスをリセットする
-ctx.rect(160, 10, 100, 40); //ctxのサイズ 円弧の作成に用いる
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)"; //ctxのカラー
-ctx.stroke(); //枠の色変更
-ctx.closePath(); //最終座標と開始座標を結び閉じる
+// ctx.beginPath(); //現在のパスをリセットする
+// ctx.rect(20, 40, 50, 50); //ctxのサイズ rect 四角形を作成に用いる
+// ctx.fillStyle = "#FF0000"; //ctxのカラー
+// ctx.fill(); //塗り潰し
+// ctx.closePath(); //最終座標と開始座標を結び閉じる
+
+// ctx.beginPath(); //現在のパスをリセットする
+// ctx.arc(240, 160, 20, 0, Math.PI * 2, false); //ctxのサイズ 円弧の作成に用いる
+// ctx.fillStyle = "green"; //ctxのカラー
+// ctx.fill(); //塗り潰し
+// ctx.closePath(); //最終座標と開始座標を結び閉じる
+
+// ctx.beginPath(); //現在のパスをリセットする
+// ctx.rect(160, 10, 100, 40); //ctxのサイズ 円弧の作成に用いる
+// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)"; //ctxのカラー
+// ctx.stroke(); //枠の色変更
+// ctx.closePath(); //最終座標と開始座標を結び閉じる
---
+++ js/test.js
@@ -0,0 +1,25 @@
+// function keyDownHandler(e) {
+//   // keyDown ボタンを押している時
+//   if (e.key == "Right" || e.key == "ArrowRight") {
+//     //右矢印ArrowRight 右矢印が押されたらtrue
+//     //右はArrowRightと対応しているがIE/Edgeが対応していない場合があるためRightも追加している
+//     rightPressed = true;
+//   } else if (e.key == "Left" || e.key == "ArrowLeft") {
+//     //左矢印ArrowLeft 左矢印が押されたらfalse
+//     //左はArrowLeftと対応しているがIE/Edgeが対応していない場合があるためLeftも追加している
+//     leftPressed = true;
+//   }
+// }
+
+// function keyUpHandler(e) {
+//   //keyUp ボタンを押していない時
+//   if (e.key == "Right" || e.key == "ArrowRight") {
+//     //左矢印ArrowRight 左矢印が押されたらfalse
+//     //右はArrowRightと対応しているがIE/Edgeが対応していない場合があるためRightも追加している
+//     rightPressed = false;
+//   } else if (e.key == "Left" || e.key == "ArrowLeft") {
+//     //左矢印ArrowRight 左矢印が押されたらfalse
+//     //左はArrowLeftと対応しているがIE/Edgeが対応していない場合があるためLeftも追加している
+//     leftPressed = false;
+//   }
+// }
